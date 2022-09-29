4dd240fbea4789a263b60ce7f284c73eb9622f35
Manoj NB <manojnb@amazon.com>
2022-09-29 16:09:17 -0700

    wip: android request permission now starts a new activity

--- packages/notifications/push/amplify_push_notifications_pinpoint_android/android/src/main/kotlin/com/amazonaws/amplify/amplify_push_notifications_pinpoint/amplify_push_notifications_pinpoint_android/AmplifyPushNotificationsPinpointAndroidPlugin.kt
+++ packages/notifications/push/amplify_push_notifications_pinpoint_android/android/src/main/kotlin/com/amazonaws/amplify/amplify_push_notifications_pinpoint/amplify_push_notifications_pinpoint_android/AmplifyPushNotificationsPinpointAndroidPlugin.kt
@@ -21,13 +21,8 @@ package com.amazonaws.amplify.amplify_push_notifications_pinpoint.amplify_push_n
 import android.Manifest
 import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
-import android.os.Build
-import androidx.activity.result.contract.ActivityResultContracts
+import android.content.Intent
 import androidx.annotation.NonNull
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.amazonaws.amplify.amplify_core.AtomicResult
 import com.amplifyframework.core.Amplify
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -39,11 +34,12 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 
-public class AmplifyPushNotificationsPinpointAndroidPlugin : FlutterPlugin, ActivityAware, MethodCallHandler, AppCompatActivity() {
+public class AmplifyPushNotificationsPinpointAndroidPlugin : FlutterPlugin, ActivityAware, MethodCallHandler{
 
   private lateinit var channel: MethodChannel
   private var mainActivity: Activity? = null
   private lateinit var context: Context
+  private val LOG = Amplify.Logging.forNamespace("amplify:flutter:notifications_pinpoint")
 
   override fun onAttachedToEngine(
           @NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
@@ -57,9 +53,6 @@ public class AmplifyPushNotificationsPinpointAndroidPlugin : FlutterPlugin, Acti
     context = flutterPluginBinding.applicationContext
   }
 
-  companion object {
-    val LOG = Amplify.Logging.forNamespace("amplify:flutter:notifications_pinpoint")
-  }
 
   // Handle methods received via MethodChannel
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull _result: Result) {
@@ -67,7 +60,10 @@ public class AmplifyPushNotificationsPinpointAndroidPlugin : FlutterPlugin, Acti
 
     when (call.method) {
       "requestMessagingPermission" -> {
-        askNotificationPermission()
+        LOG.info("Asking for permission ")
+        mainActivity!!.startActivity(Intent(context,PermissionActivity::class.java))
+
+//        askNotificationPermission()
         result.success(null)
       }
       else -> result.notImplemented()
@@ -94,75 +90,5 @@ public class AmplifyPushNotificationsPinpointAndroidPlugin : FlutterPlugin, Acti
     channel.setMethodCallHandler(null)
   }
 
-  // Declare the launcher at the top of your Activity/Fragment:
-  private val requestPermissionLauncher = registerForActivityResult(
-    ActivityResultContracts.RequestPermission()
-  ) { isGranted: Boolean ->
-    if (isGranted) {
-      // FCM SDK (and your app) can post notifications.
-      LOG.debug("App can post push notifications 2 ")
-    } else {
-      // TODO: Inform user that that your app will not show notifications.
-    }
-  }
-
-  private fun askNotificationPermission() {
-    if (Build.VERSION.SDK_INT >= 33) {
-
-//       This is only necessary for API level >= 33 (TIRAMISU)
-      if (ContextCompat.checkSelfPermission(context,Manifest.permission.POST_NOTIFICATIONS) ==
-        PackageManager.PERMISSION_GRANTED
-      ) {
-        // FCM SDK (and your app) can post notifications.
-        LOG.debug("App can post push notifications 1 ")
-      }
-      else if (mainActivity!!.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-        // TODO: display an educational UI explaining to the user the features that will be enabled
-        //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-        //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-        //       If the user selects "No thanks," allow the user to continue without notifications.
-        LOG.debug("App should ask for permissions")
-      }
-      else {
-        // Directly ask for the permission
-//        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-        mainActivity!!.requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
-
-      }
-    }
-  }
-
-  override fun onRequestPermissionsResult(requestCode: Int,
-                                          permissions: Array<String>, grantResults: IntArray) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    when (requestCode) {
-      0 -> {
-        // If request is cancelled, the result arrays are empty.
-        if ((grantResults.isNotEmpty() &&
-                  grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-          // Permission is granted. Continue the action or workflow
-          // in your app.
-          LOG.debug("Permission is granted.")
-
-        } else {
-          // Explain to the user that the feature is unavailable because
-          // the features requires a permission that the user has denied.
-          // At the same time, respect the user's decision. Don't link to
-          // system settings in an effort to convince the user to change
-          // their decision.
-
-          LOG.debug("Permission is denied.")
-
-        }
-        return
-      }
-
-      // Add other 'when' lines to check for other
-      // permissions this app might request.
-      else -> {
-        // Ignore all other requests.
-      }
-    }
-  }
 
 }
