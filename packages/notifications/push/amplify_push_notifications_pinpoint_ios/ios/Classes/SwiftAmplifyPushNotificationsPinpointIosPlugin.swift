45282b96ce65eb3aa21a2249301522246b5c013e
Manoj NB <manojnb@amazon.com>
2022-09-30 13:58:14 -0700

    wip: adds ios poc to request permission

--- packages/notifications/push/amplify_push_notifications_pinpoint_ios/ios/Classes/SwiftAmplifyPushNotificationsPinpointIosPlugin.swift
+++ packages/notifications/push/amplify_push_notifications_pinpoint_ios/ios/Classes/SwiftAmplifyPushNotificationsPinpointIosPlugin.swift
@@ -1,14 +1,36 @@
 import Flutter
 import UIKit
+import amplify_core
 
 public class SwiftAmplifyPushNotificationsPinpointIosPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amplify_push_notifications_pinpoint_ios", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.amazonaws.amplify/notifications_pinpoint", binaryMessenger: registrar.messenger())
     let instance = SwiftAmplifyPushNotificationsPinpointIosPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      let result = AtomicResult(result, call.method)
+
+      innerHandle(method: call.method, callArgs: call.arguments as Any?, result: result)
+
   }
+    public func innerHandle(method: String, callArgs: Any?, result: @escaping FlutterResult) {
+        switch method {
+        case "requestMessagingPermission": do {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+
+                if let error = error {
+                    // Handle the error here.
+                }
+
+                // Enable or disable features based on the authorization.
+            }
+        }
+
+        default:
+                   result(FlutterMethodNotImplemented)
+               }
+    }
 }
