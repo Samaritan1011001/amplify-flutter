b780c190ccfb8a68fd336e59be33932a52de22ed
Manoj NB <manojnb@amazon.com>
2023-02-06 18:09:59 -0800

    feat(push): adds license headers to new files

--- packages/amplify_core/lib/src/types/notifications/notifications_types.dart
+++ packages/amplify_core/lib/src/types/notifications/notifications_types.dart
@@ -1,2 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 export 'push_notifications/push_permission_request_status.dart';
 export 'push_notifications/remote_push_message.dart';
